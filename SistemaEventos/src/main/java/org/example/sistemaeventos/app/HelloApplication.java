d9bf8981a2670845f0a3c2e106ec7945d82c5ce5
Sebastian <darwin.caiza@epn.edu.ec>
2026-07-12 16:58:00 -0500

    feat: agregar Login.fxml con botón de registro y estructura inicial

--- SistemaEventos/src/main/java/org/example/sistemaeventos/app/HelloApplication.java
+++ SistemaEventos/src/main/java/org/example/sistemaeventos/app/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/sistemaeventos/view/registro.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/sistemaeventos/view/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- SistemaEventos/src/main/java/org/example/sistemaeventos/controller/LoginController.java
+++ SistemaEventos/src/main/java/org/example/sistemaeventos/controller/LoginController.java
@@ -1,4 +1,54 @@
 package org.example.sistemaeventos.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Label lblMensaje;
+
+    @FXML
+    public void iniciarSesion(){
+
+        String usuario = txtUsuario.getText();
+        String password = txtPassword.getText();
+
+        if(usuario.isEmpty() || password.isEmpty()){
+            lblMensaje.setText("Complete todos los campos");
+        }else{
+            lblMensaje.setText("Validando usuario...");
+        }
+    }
+
+    @FXML
+    public void abrirRegistro(){
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "/org/example/sistemaeventos/view/registro.fxml")
+            );
+            Parent root = loader.load();
+            Stage stage = (Stage) txtUsuario.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
