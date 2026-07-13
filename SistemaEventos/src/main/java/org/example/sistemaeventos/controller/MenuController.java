130d01ee7b405055aebcc83fa829e396c00b3825
Leandro <leandro.jacho2003@gmail.com>
2026-07-12 19:22:38 -0500

    inicios declaracion de variables y clase menu controller

--- SistemaEventos/src/main/java/org/example/sistemaeventos/controller/MenuController.java
+++ SistemaEventos/src/main/java/org/example/sistemaeventos/controller/MenuController.java
@@ -1,6 +1,62 @@
 package org.example.sistemaeventos.controller;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 
 public class MenuController {
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtTipo;
+
+    @FXML
+    private TextField txtInicio;
+
+    @FXML
+    private TextField txtFin;
+
+    @FXML
+    private DatePicker dpFecha;
+
+    //@FXML
+    //private TableView<Eventos> tblEventos;
+
+
+    //@FXML
+    //private TableColumn<Eventos, String> colNombre;
+
+    //@FXML
+    //private TableColumn<Eventos, String> colTipo;
+
+    //@FXML
+    //private TableColumn<Eventos, String> colInicio;
+
+    //@FXML
+    //private TableColumn<Eventos, String> colFin;
+
+    //@FXML
+    //private TableColumn<Eventos, String> colFecha;
+
+
+    @FXML
+    public void registrar(){
+
+    }
+
+    @FXML
+    public void eliminar(){
+
+    }
+
+    @FXML
+    public void actualizar(){
+
+    }
 }
